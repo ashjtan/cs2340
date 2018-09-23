91ec909c16a2034f02a3f8bc61999702e94056ac
Ashley Tan <tan.ashley32@gmail.com>
2018-09-23 16:42:59 -0400

    #25 switched onClick handler from hardcoded implementation to xml attribute

--- app/src/main/java/cs2340/group61/doughnation/LoginActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/LoginActivity.java
@@ -8,42 +8,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActionBar actionbar;
+    Button loginButton, registerButton;
+    EditText userField, passField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Instantiating loginButton
-        Button loginButton = (Button)findViewById(R.id.login_button);
+        //Instantiating xml elements
+        loginButton = (Button)findViewById(R.id.login_button);
+        registerButton = (Button) findViewById(R.id.register_Button);
+        userField = (EditText) findViewById(R.id.username);
+        passField = (EditText) findViewById(R.id.password);
 
-        //Instantiating registerButton
-        Button registerButton = (Button) findViewById(R.id.register_Button);
 
         //Setting some actionbar characteristics
         //Changes color and text in the actionbar
         actionbar = getSupportActionBar();
         actionbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF4A4A")));
         actionbar.setTitle("DoughNation");
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, HomePageActivity.class));
-            }
-        });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,
-                        RegistrationActivity.class));
-            }
-        });
     }
 
+
+
+
+    //EVENT HANDLERS
+    public void login(View v) {     //logs in if valid credentials
+        if (validLogin()) {
+            startActivity(new Intent(LoginActivity.this, HomePageActivity.class));
+        }
+        else {
+            //TODO: add error message
+        }
+    }
+
+    public void switchToRegister(View v) {      //switches to register page
+        startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+    }
 }
