8776e939da3eb46651cd16d1041e12e29b28d88a
Ashley Tan <tan.ashley32@gmail.com>
2018-09-29 23:42:18 -0400

    #33 populated acctType spinner

---
+++ app/src/main/java/cs2340/group61/doughnation/domain/Organization.java
@@ -0,0 +1,6 @@
+package cs2340.group61.doughnation.domain;
+
+//Sample organizations to assign managers/location employees
+public enum Organization {
+    HUMANE_SOCIETY, GOODWILL, DOCTORS_WITHOUT_BORDERS;
+}
