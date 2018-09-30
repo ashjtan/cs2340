2e78a8d0fd8064dffc888b4442d13958a353ee46
Ashley Tan <tan.ashley32@gmail.com>
2018-09-30 19:01:37 -0400

    #32 refactored classes

---
+++ app/src/main/java/cs2340/group61/doughnation/model/domain/accounts/OrganizationManager.java
@@ -0,0 +1,11 @@
+package cs2340.group61.doughnation.model.domain.accounts;
+
+public class OrganizationManager extends User {
+    public OrganizationManager() {
+        super();
+    }
+
+    public OrganizationManager(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
