014d04e0654a7509a14c98368ac88bd3571f27f6
Ashley Tan <tan.ashley32@gmail.com>
2018-09-29 23:34:46 -0400

    #33 populated acctType spinner

--- app/src/main/java/cs2340/group61/doughnation/controller/RegistrationActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/RegistrationActivity.java
@@ -7,10 +7,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import cs2340.group61.doughnation.R;
+import cs2340.group61.doughnation.domain.AccountType;
 import cs2340.group61.doughnation.domain.Utils;
 
 import static cs2340.group61.doughnation.domain.Utils.isNotEmpty;
@@ -34,7 +37,10 @@ public class RegistrationActivity extends AppCompatActivity {
         actionbar.setTitle("DoughNation");
 
 
-
+        //Populates spinner w/ account types
+        ArrayAdapter<String> acctTypeAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, AccountType.values());
+        acctTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        acctType.setAdapter(acctTypeAdapter);
     }
 
 
---
+++ app/src/main/java/cs2340/group61/doughnation/domain/AccountType.java
@@ -0,0 +1,5 @@
+package cs2340.group61.doughnation.domain;
+
+public enum AccountType {
+    ADMIN, GENERAL_USER, LOCATION_EMPLOYEE, LOCATION_MANAGER;
+}
