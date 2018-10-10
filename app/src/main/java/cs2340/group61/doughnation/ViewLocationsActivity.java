51dfc429f39f7f4c5730fb3b467ae9e7f14a9e87
haleydabbs <hdabbs3@gatech.edu>
2018-10-10 18:57:55 -0400

    Modified Main Page UI

---
+++ app/src/main/java/cs2340/group61/doughnation/ViewLocationsActivity.java
@@ -0,0 +1,13 @@
+package cs2340.group61.doughnation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLocationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_locations);
+    }
+}
