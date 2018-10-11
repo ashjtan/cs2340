360f772abeea6a306add1d6048e8c54192913c46
haleydabbs <hdabbs3@gatech.edu>
2018-10-10 20:19:18 -0400

    Added Recycler view container
    Edits made inside activity_view_locations.xml

--- app/src/main/java/cs2340/group61/doughnation/controller/ViewLocationsActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/ViewLocationsActivity.java
@@ -2,14 +2,26 @@ package cs2340.group61.doughnation.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.LinkedList;
 
 import cs2340.group61.doughnation.R;
 
 public class ViewLocationsActivity extends AppCompatActivity {
 
+    //This is a temporary LinkedList to fill the
+    private final LinkedList<String> locationDataEx = new LinkedList<>();
+    private int counter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_locations);
+
+        for (int i = 0; i < 20; i++) {
+            locationDataEx.addLast("Word " + counter++);
+            Log.d("WordList", locationDataEx.getLast());
+        }
     }
 }
