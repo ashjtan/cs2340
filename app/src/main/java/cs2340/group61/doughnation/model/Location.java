52ffd105d2b3efc8992080acd03eb7a61d50d979
bnations3 <bnations3@gatech.edu>
2018-10-14 16:52:20 -0400

    External Database
    added external database and access classes

---
+++ app/src/main/java/cs2340/group61/doughnation/model/Location.java
@@ -0,0 +1,61 @@
+package cs2340.group61.doughnation.model;
+
+public class Location {
+    public int Key;
+    public String Name;
+    public String Latitude;
+    public String Longitude;
+    public String Address;
+    public String City;
+    public String State;
+    public String Zip;
+    public String Type;
+    public String Phone;
+    public String Website;
+
+    public Location(){}
+
+    public void setKey(int key) {
+        Key = key;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setLatitude(String latitude) {
+        Latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        Longitude = longitude;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public void setZip(String zip) {
+        Zip = zip;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public void setWebsite(String website) {
+        Website = website;
+    }
+}
