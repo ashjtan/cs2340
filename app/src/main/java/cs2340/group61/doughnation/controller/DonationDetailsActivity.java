4286d8aab5ad326f74a959d21d32137a8f5a6588
haleydabbs <hdabbs3@gatech.edu>
2018-10-21 18:31:21 -0400

    Linked logout/back buttons on Donation Details View

--- app/src/main/java/cs2340/group61/doughnation/controller/DonationDetailsActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/DonationDetailsActivity.java
@@ -1,7 +1,10 @@
 package cs2340.group61.doughnation.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cs2340.group61.doughnation.R;
 
@@ -11,5 +14,29 @@ public class DonationDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_details);
+
+        //Button to go back to donation recyclerview page
+        Button backbutton = (Button) findViewById(R.id.back_donation_view_button);
+
+        //Button to logout
+        Button logoutButton = (Button) findViewById(R.id.return_login_Button);
+
+        //Method to go back to LocationDetailActivity
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DonationDetailsActivity.this,
+                        ViewDonationsActivity.class));
+            }
+        });
+
+        //Method to logout
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DonationDetailsActivity.this,
+                        LoginActivity.class));
+            }
+        });
     }
 }
