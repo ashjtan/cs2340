4cd08d886f436deb042f1f05baad96329771fdcb
Jasmine Chacko <jasminevchacko@gmail.com>
2018-10-26 00:21:11 -0400

    Merge remote-tracking branch 'remotes/origin/UI-Design' into Jasmine
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/vcs.xml
    
    created DonationCategory enum & finished spinner for adding a donation

--- app/src/main/java/cs2340/group61/doughnation/controller/AddDonationActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/AddDonationActivity.java
@@ -5,17 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import cs2340.group61.doughnation.R;
+import cs2340.group61.doughnation.model.DonationCategory;
+
+import static cs2340.group61.doughnation.R.layout.activity_add_donation;
 
 public class AddDonationActivity extends AppCompatActivity {
 
     private static final String TAG = "ViewDonationsActivity";
 
+    private Spinner donationCategorySpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_donation);
+        setContentView(activity_add_donation);
 
         //Button to go back to locationDetailActivity page
         Button backbutton = (Button) findViewById(R.id.back_button);
@@ -25,6 +32,14 @@ public class AddDonationActivity extends AppCompatActivity {
 
         //Button to go to AddDonationActivity
         Button addDonationButton = (Button) findViewById(R.id.done_button);
+
+        //Spinner for donation category types
+        donationCategorySpinner = (Spinner) findViewById(R.id.donation_type_spinner);
+
+        //populate donation category spinner
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, DonationCategory.values());
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        donationCategorySpinner.setAdapter(categoryAdapter);
 
         //Method to go back to LocationDetailActivity
         backbutton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/cs2340/group61/doughnation/model/DonationCategory.java
@@ -0,0 +1,5 @@
+package cs2340.group61.doughnation.model;
+
+public enum DonationCategory {
+    CLOTHING, HAT, KITCHEN, ELECTRONICS, HOUSEHOLD, OTHER;
+}
