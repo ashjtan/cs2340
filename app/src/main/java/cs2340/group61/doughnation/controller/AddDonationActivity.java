9cf517a703a2a51cc65ca1f1e862d561c9b89160
bnations3 <bnations3@gatech.edu>
2018-10-30 02:33:15 -0400

    add donation implementation
    adding donations

--- app/src/main/java/cs2340/group61/doughnation/controller/AddDonationActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/AddDonationActivity.java
@@ -1,9 +1,12 @@
 package cs2340.group61.doughnation.controller;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,9 +15,14 @@ import java.util.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import cs2340.group61.doughnation.model.domain.Donation;
+import cs2340.group61.doughnation.model.Location;
 
 import java.util.ArrayList;
 
@@ -23,16 +31,20 @@ import cs2340.group61.doughnation.R;
 public class AddDonationActivity extends AppCompatActivity {
 
     private static final String TAG = "ViewDonationsActivity";
-    public DatabaseReference databaseDonations;
+    private DatabaseReference databaseLocations;
+    private DatabaseReference databaseDonations;
+    private ArrayList<Location> locationList = new ArrayList<>();
+    private ArrayList<String> nameList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
 
-<<<<<<< HEAD
+        databaseLocations = FirebaseDatabase.getInstance().getReference("locations");
         databaseDonations = FirebaseDatabase.getInstance().getReference("donations");
-=======
+
         //Arraylist to fill Locations spinner
         //Bri add code here
 
@@ -45,7 +57,31 @@ public class AddDonationActivity extends AppCompatActivity {
         cat_array.add("Hat");
         cat_array.add("Other");
 
->>>>>>> origin/UI-Design
+        Spinner location = (Spinner) findViewById(R.id.location_selection_spinner);
+        EditText name = (EditText) findViewById(R.id.add_name);
+        EditText value = (EditText) findViewById(R.id.add_value);
+        Spinner type = (Spinner) findViewById(R.id.donation_type_spinner);
+        EditText description = (EditText) findViewById(R.id.add_description);
+
+        ArrayAdapter<String> adapterCat = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, cat_array);
+        adapterCat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner catItems = (Spinner) findViewById(R.id.donation_type_spinner);
+        catItems.setAdapter(adapterCat);
+
+        //onStart();
+
+        //for (Location lo: locationList) {
+        //    nameList.add(lo.name);
+        //}
+
+
+        //ArrayAdapter<String> adapterLoc = new ArrayAdapter<String>(
+        //        this, android.R.layout.simple_spinner_item, nameList);
+        //adapterLoc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Spinner locationItems = (Spinner) findViewById(R.id.location_selection_spinner);
+        //locationItems.setAdapter(adapterLoc);
+
 
         //Button to go back to locationDetailActivity page
         Button backbutton = (Button) findViewById(R.id.back_button);
@@ -56,9 +92,6 @@ public class AddDonationActivity extends AppCompatActivity {
         //Button to go to AddDonationActivity
         Button addDonationButton = (Button) findViewById(R.id.done_button);
 
-        final EditText name = (EditText) findViewById(R.id.add_name);
-
-        final EditText description = (EditText) findViewById(R.id.add_description);
 
         
         //Method to go back to LocationDetailActivity
@@ -90,8 +123,44 @@ public class AddDonationActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    private void addDonation() {
+        databaseLocations.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                locationList.clear();
+
+                for(DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    Location location = locationSnapshot.getValue(Location.class);
+
+                    locationList.add(location);
+                }
+
+                for (Location lo: locationList) {
+                    nameList.add(lo.name);
+                }
+
+
+                ArrayAdapter<String> adapterLoc = new ArrayAdapter<String>(AddDonationActivity.this, android.R.layout.simple_spinner_item, nameList);
+                adapterLoc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                Spinner locationItems = (Spinner) findViewById(R.id.location_selection_spinner);
+                locationItems.setAdapter(adapterLoc);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    public void addDonation() {
         String id = databaseDonations.push().getKey();
 
         final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
@@ -99,7 +168,7 @@ public class AddDonationActivity extends AppCompatActivity {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         String time = sdf.format(timestamp);
 
-        Spinner location = (Spinner) findViewById(R.id.location_selection_spinner);
+        Spinner locationItems = (Spinner) findViewById(R.id.location_selection_spinner);
         EditText name = (EditText) findViewById(R.id.add_name);
         EditText value = (EditText) findViewById(R.id.add_value);
         Spinner type = (Spinner) findViewById(R.id.donation_type_spinner);
@@ -107,7 +176,7 @@ public class AddDonationActivity extends AppCompatActivity {
 
         Donation donation = new Donation();
         donation.setTitle(name.getText().toString());
-        donation.setLocation(location.getSelectedItem().toString());
+        donation.setLocation(locationItems.getSelectedItem().toString());
         donation.setCategory(type.getSelectedItem().toString());
         donation.setFulldescription(description.getText().toString());
         donation.setShortdescription(description.getText().toString());
