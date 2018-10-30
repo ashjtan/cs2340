5cdfc24366a8f8823115c2cd66c6be8e38d1a51c
haleydabbs <hdabbs3@gatech.edu>
2018-10-30 00:19:41 -0400

    Updated location details xml

--- app/src/main/java/cs2340/group61/doughnation/controller/LocationDetailsActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/LocationDetailsActivity.java
@@ -65,19 +65,6 @@ public class LocationDetailsActivity extends AppCompatActivity {
             }
         });
 
-        //Click listener to go to view donations page
-//        viewDonations.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(LocationDetailsActivity.this,
-//                        ViewDonationsActivity.class));
-//            }
-//        });
-<<<<<<< HEAD
-        
-        getIncomingIntent();
-=======
-
     }
 
     @Override
@@ -109,7 +96,6 @@ public class LocationDetailsActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
->>>>>>> origin/Bri
     }
 
 
