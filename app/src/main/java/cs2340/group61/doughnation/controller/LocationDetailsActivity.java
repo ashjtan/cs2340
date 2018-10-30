fa8a8f91d5dc2f0debca172c45018d703e2c6774
haleydabbs <hdabbs3@gatech.edu>
2018-10-29 23:50:22 -0400

    Update app/src/main/java/cs2340/group61/doughnation/controller/LocationDetailsActivity.java

--- app/src/main/java/cs2340/group61/doughnation/controller/LocationDetailsActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/LocationDetailsActivity.java
@@ -1,6 +1,7 @@
 package cs2340.group61.doughnation.controller;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import cs2340.group61.doughnation.controller.ViewLocationsActivity;
+
 import cs2340.group61.doughnation.model.Location;
 
 import cs2340.group61.doughnation.R;
 
 public class LocationDetailsActivity extends AppCompatActivity {
+
+    public ArrayList<Location> locationList = new ArrayList<>();
+    public DatabaseReference databaseLocations;
+    public String locationName;
 
     private static final String TAG = "LocationDetailActivity";
 
@@ -20,6 +35,8 @@ public class LocationDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_details);
+
+        databaseLocations = FirebaseDatabase.getInstance().getReference("locations");
 
         //Create button to go back to location list screen
         Button backButton = (Button) findViewById(R.id.back_location_button);
@@ -56,8 +73,43 @@ public class LocationDetailsActivity extends AppCompatActivity {
 //                        ViewDonationsActivity.class));
 //            }
 //        });
+<<<<<<< HEAD
         
         getIncomingIntent();
+=======
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseLocations.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                locationList.clear();
+
+                for(DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    Location location = locationSnapshot.getValue(Location.class);
+
+                    locationList.add(location);
+                }
+                getIncomingIntent();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+>>>>>>> origin/Bri
     }
 
 
@@ -69,16 +121,21 @@ public class LocationDetailsActivity extends AppCompatActivity {
 
             String locationName = getIntent().getStringExtra("location_name");
 
+            this.locationName = locationName;
             setLocationDetails(locationName);
         }
     }
 
-
     //Set details from information passed in from RecyclerViewAdapter
     private void setLocationDetails(String locationName) {
         Log.d(TAG, "setLocationDetails: Setting Location name and details");
-
-        Location loc = new Location();
+        int index = 0;
+        for (Location location: locationList) {
+            if (location.name.equals(locationName)) {
+                index = locationList.indexOf(location);
+            }
+        }
+        Location loc = locationList.get(index);
 
         TextView name = findViewById(R.id.location_name);
         name.setText(locationName.toUpperCase());
