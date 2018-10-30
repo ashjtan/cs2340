42b8e7a62840275ddbb75d69156a20b9bd56aaa2
bnations3 <bnations3@gatech.edu>
2018-10-29 23:39:29 -0400

    View Location Details
    View Location Details

--- app/src/main/java/cs2340/group61/doughnation/controller/ViewLocationsActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/ViewLocationsActivity.java
@@ -15,7 +15,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -113,5 +112,9 @@ public class ViewLocationsActivity extends AppCompatActivity {
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerview.setLayoutManager(layoutManager);
+    }
+
+    protected ArrayList<Location> getLocationList() {
+        return locationList;
     }
 }
