d26f68bc62685111530d3321bf81a428c9b1dfce
Ashley Tan <tan.ashley32@gmail.com>
2018-11-02 02:45:57 -0400

    added db instances to AppData

--- app/src/main/java/cs2340/group61/doughnation/model/AppData.java
+++ app/src/main/java/cs2340/group61/doughnation/model/AppData.java
@@ -1,11 +1,22 @@
 package cs2340.group61.doughnation.model;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import cs2340.group61.doughnation.model.domain.accounts.User;
 
 public class AppData {
+    public static DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference().child("users");
+    //public static DatabaseReference dbOrgs = FirebaseDatabase.getInstance().getReference().child("organizations");
+    public static DatabaseReference dbLocations = FirebaseDatabase.getInstance().getReference().child("locations");
+    public static DatabaseReference dbDonations = FirebaseDatabase.getInstance().getReference().child("donations");
+
+
+
+
     private static List<User> accounts = new ArrayList<User>();
 
     public static List<User> getAccounts() {
