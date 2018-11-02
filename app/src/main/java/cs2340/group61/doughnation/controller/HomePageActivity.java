d70febb0b6187395c17f5574b1da751950f8dafb
Ashley Tan <tan.ashley32@gmail.com>
2018-11-02 04:10:56 -0400

    cleaned up HomePageActivity
    moved onClicks to xml

--- app/src/main/java/cs2340/group61/doughnation/controller/HomePageActivity.java
+++ app/src/main/java/cs2340/group61/doughnation/controller/HomePageActivity.java
@@ -8,12 +8,17 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 import cs2340.group61.doughnation.R;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private ActionBar actionbar;
+    private Button toLogin;
+    private Button myLocations;
+    private Button toAddDonations;
+    private Button toSearchDonations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,52 +31,26 @@ public class HomePageActivity extends AppCompatActivity {
         actionbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF4A4A")));
         actionbar.setTitle("DoughNation");
 
-        //Instantiating toLogin button to return to login from Reg. page
-        Button toLogin = (Button) findViewById(R.id.return_login_Button);
-
-        //Instantiating myLocations button to go to Locations RecyclerView page
-        Button myLocations = (Button) findViewById(R.id.locationsButton);
-
-        //Instantiating toAddDonation button to go to add donations page
-        Button toAddDonations = (Button) findViewById(R.id.add_donation_button);
-
-        //Instantiating toSearchDonations button to go to view donations page
-        Button toSearchDonations = (Button) findViewById(R.id.search_donations_button);
-
-        //Method to return to main login screen
-        toLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomePageActivity.this, LoginActivity.class));
-            }
-        });
-
-        //Method to go to Recycler View page containing list of locations
-        myLocations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomePageActivity.this,
-                        ViewLocationsActivity.class));
-            }
-        });
-
-        //Method to go to Add Donation Activity
-        toAddDonations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomePageActivity.this,
-                        AddDonationActivity.class));
-            }
-        });
-
-        //Method to go to Search Donations Activity
-        toSearchDonations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomePageActivity.this,
-                        ViewDonationsActivity.class));
-            }
-        });
+        toLogin = (Button) findViewById(R.id.btnLogout);
+        myLocations = (Button) findViewById(R.id.btnLocations);
+        toAddDonations = (Button) findViewById(R.id.btnAddDonations);
+        toSearchDonations = (Button) findViewById(R.id.btnSearchDonations);
     }
 
+    //EVENT HANDLERS
+    public void switchToLogin(View v) {             //switches to login page
+        startActivity(new Intent(HomePageActivity.this, LoginActivity.class));
+    }
+
+    public void switchToMyLocations(View v) {      //switches to my locations page
+        startActivity(new Intent(HomePageActivity.this, ViewLocationsActivity.class));
+    }
+
+    public void switchToAddDonation(View v) {      //switches to add donation page
+        startActivity(new Intent(HomePageActivity.this, AddDonationActivity.class));
+    }
+
+    public void switchToSearchDonations(View v) {      //switches to search donations page
+        startActivity(new Intent(HomePageActivity.this, ViewDonationsActivity.class));
+    }
 }
