1538bea6a2df16f4f12de91d6ca8c6af331b68eb
Ashley Tan <tan.ashley32@gmail.com>
2018-11-02 02:44:28 -0400

    moved utils to new package

---
+++ app/src/main/java/cs2340/group61/doughnation/model/utils/Utils.java
@@ -0,0 +1,47 @@
+package cs2340.group61.doughnation.model.utils;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+public class Utils {
+
+    /**
+     * Checks that strings are not null/empty.
+     * Primarily for checking that EditText form fields are filled out.
+     * @param strings Strings to check.
+     */
+    public static boolean isNotEmpty(String... strings) {
+        for (String s : strings) {
+            if (s == null || s.trim().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Sets string values to empty.
+     * Primarily for setting String vars from EditText back to blank for rechecking vals.
+     * @param strings Strings to set to empty.
+     */
+    public static void clearTextVals(String... strings) {
+        for (String s : strings) {
+            s = "";
+        }
+    }
+
+    /**
+     * Shows custom error dialog.
+     * @param message
+     * @param title
+     * @param activity Should always be called with "this" as param.
+     */
+    public static void showDialog(String message, String title, AppCompatActivity activity) {
+        AlertDialog.Builder alert  = new AlertDialog.Builder(activity);
+        alert.setMessage(message);
+        alert.setTitle(title);
+        alert.setPositiveButton("OK", null);
+
+        alert.create().show();
+    }
+}
