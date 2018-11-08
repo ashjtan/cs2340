200f3db6922ad3bbeffdcc3f67681afe793c64d6
haleydabbs <hdabbs3@gatech.edu>
2018-11-08 00:24:49 -0500

    Commented out unused variables

--- app/src/main/java/cs2340/group61/doughnation/model/Location.java
+++ app/src/main/java/cs2340/group61/doughnation/model/Location.java
@@ -1,61 +1,61 @@
 package cs2340.group61.doughnation.model;
 
 public class Location {
-    private String id;
+    //private String id;
     public String name;
     public String latitude;
     public String longitude;
     public String address;
-    private String city;
-    public String state;
-    public String zip;
+    //private String city;
+    //public String state;
+    //public String zip;
     public String type;
     public String phone;
-    public String website;
+    //public String website;
 
     public Location(){}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+//    public void setId(String id) {
+//        this.id = id;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
+//    public void setLatitude(String latitude) {
+//        this.latitude = latitude;
+//    }
 
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
+//    public void setLongitude(String longitude) {
+//        this.longitude = longitude;
+//    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
 
-    public void setState(String state) {
-        this.state = state;
-    }
+//    public void setState(String state) {
+//        this.state = state;
+//    }
 
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
+//    public void setZip(String zip) {
+//        this.zip = zip;
+//    }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
 
-    public void setWebsite(String website) {
-        this.website = website;
-    }
+//    public void setWebsite(String website) {
+//        this.website = website;
+//    }
 }
